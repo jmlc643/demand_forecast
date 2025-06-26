8b0ebfb174591b1197d7160f031e8d4f9d4ce445
jmlc643 <josemarialuyocampos@gmail.com>
2025-06-26 18:58:20 -0500

    deploy(Back): Se añadio un middleware

--- back.py
+++ back.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import DemandForecastModel
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
+)
+
 model = DemandForecastModel()
 
 class PredictionInput(BaseModel):
